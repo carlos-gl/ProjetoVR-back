4c946b8973c4a7bfee8a6baa0c1a192f8b575d0a
carlos-gl <carloslins170@gmail.com>
2023-04-13 15:20:44 -0300

    Update get.rb
    add comments

--- features/step_definitions/get.rb
+++ features/step_definitions/get.rb
@@ -1,17 +1,26 @@
 Dado('que o usuario consulte as informações dos produtos e estabelecimentos') do
+    # atribuo a variavel 'getUrl' a url da api que será consultada.
     @getUrl = 'https://portal.vr.com.br/api-web/comum/enumerations/VRPAT'
   end
   
   Quando('ele fazer uma pesquisa') do
-    @resultGet = HTTParty.get(@getUrl)
+    # atribuo a variavel 'responseGet' o response retornado no get.
+    @responseGet = HTTParty.get(@getUrl)
   end
   
   Entao('uma lista de valores da chave typeOfEstablishment deve ser retornado') do
-    expect(@resultGet).to include("typeOfEstablishment")
-    @responseParsed = @resultGet.parsed_response["typeOfEstablishment"].sample
-    @randomResult = "Informações de um dos estabelecimentos\n" \
+    # valido que existe a chave typeOfEstablishment no response.
+    expect(@responseGet).to include("typeOfEstablishment")
+
+    # Filtro somenete os resultados da chave typeOfEstablishment e atribuo a nova variavel 'responseParsed'.
+    @responseParsed = @responseGet.parsed_response["typeOfEstablishment"].sample
+
+    # depois de filtrado os dados crio um nova variavel 'randomResult' onde atribuo dados randomicos referentes aos valores da chave typeOfEstablishment.
+    @randomResult = "Informações de um dos estabelecimentos:\n" \
     "Chave: #{@responseParsed['key']}\n" \
     "Nome: #{@responseParsed['name']}\n" \
     "Descrição: #{@responseParsed['label']}"
+
+    # printo os valores no console.
     puts @randomResult 
   end
